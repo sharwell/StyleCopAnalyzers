fc6c691211dbeb11974faaab4616cb3ba834a513
Sam Harwell <sam@tunnelvisionlabs.com>
2014-11-19 07:59:38 -0600

    Prevent the TokenAnalysisDiagnosticAnalyzer helper analyzer from being disabled by users

--- StyleCop.Analyzers/StyleCop.Analyzers/TokenAnalysisDiagnosticAnalyzer.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers/TokenAnalysisDiagnosticAnalyzer.cs
@@ -11,8 +11,17 @@
         private static ImmutableArray<Action<SyntaxTokenAnalysisContext>> _handlers =
             ImmutableArray<Action<SyntaxTokenAnalysisContext>>.Empty;
 
+        /// <remarks>
+        /// <para>The <see cref="DiagnosticSeverity"/> must be set to <see cref="DiagnosticSeverity.Info"/> or higher,
+        /// or the analyzer will be suppressed for all files that are not currently open (even if the IDE is configured
+        /// to show messages from all files).</para>
+        ///
+        /// <para>The <see cref="WellKnownDiagnosticTags.NotConfigurable"/> tag prevents this diagnostic from appearing
+        /// in the rule editor, which is important since this analyzer implements functionality that is required by
+        /// (potentially) many other analyzers.</para>
+        /// </remarks>
         private static readonly DiagnosticDescriptor Descriptor =
-            new DiagnosticDescriptor("Internal:TokenAnalysisHelper", "TokenAnalysisHelper", string.Empty, string.Empty, DiagnosticSeverity.Info, true);
+            new DiagnosticDescriptor("Internal:TokenAnalysisHelper", "TokenAnalysisHelper", string.Empty, string.Empty, DiagnosticSeverity.Info, true, customTags: WellKnownDiagnosticTags.NotConfigurable);
 
         private static readonly ImmutableArray<DiagnosticDescriptor> _supportedDiagnostics =
             ImmutableArray.Create(Descriptor);
